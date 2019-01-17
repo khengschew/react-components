0cd7d1af63c7f84f7c650f3e24bd6cdac65643aa
khengschew <khengc@gmail.com>
2019-01-16 16:31:18 -0800

    Implement GroceryListItem class using React

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <ul>
+      <GroceryListItem item={'Tomatoes'}/>
+      <GroceryListItem item={'Ketchup'}/>
+    </ul>
+  </div>
+);
+
+var Tomatoes = () => (
+  <li>Tomatoes</li>
+);
+
+var Ketchup = () => (
+  <li>Ketchup</li>
+);
+
+// var GroceryListItem = (props) => (
+//   <li>{props.item}</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mouseover: false
+    };
+  }
+
+  onHover() {
+    console.log('this worked!');
+    this.setState({
+      mouseover: !this.state.mouseover
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.mouseover ? "bold" : "400"
+    };
+    return (
+      <li style={style} onMouseOut={this.onHover
+      .bind(this)} onMouseOver={this.onHover
+      .bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+/*
+    <TodoList />
+*/
